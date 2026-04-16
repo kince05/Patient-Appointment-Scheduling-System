512daeb441242de7cbabe4a1f346238315f08f11
kince05 <kyleince86@gmail.com>
2026-04-15 19:29:34 -0600

    Update main.py

--- Scheduling_System/main.py
+++ Scheduling_System/main.py
@@ -1,32 +1,45 @@
 import tkinter as tk
+import customtkinter as ctk
 from database import DatabaseManager
 from scheduler import Scheduler
 from gui import AppointmentGUI
 from auth import AuthService
 from login_gui import LoginGUI
+from ai_scheduler import SmartAIScheduler
 
 def main():
     db = DatabaseManager("appointments.db")
-    scheduler = Scheduler(db)
+
+    # AI model (hot-swappable)
+    ai_model = SmartAIScheduler()
+
+    scheduler = Scheduler(db, ai_model=ai_model)
     auth = AuthService(db)
 
-    # create default admin
+    # Create default admin
     try:
         db.create_user("admin", "admin123", "admin")
     except:
         pass
 
-    root = tk.Tk()
+    try:
+        db.get_or_create_doctor("Dr Smith")
+        db.get_or_create_doctor("Dr Adams")
+        db.get_or_create_doctor("Dr Lee")
+    except:
+        pass
+
+
+    root = ctk.CTk()
 
     def start_app(user):
         root.destroy()
         main_root = tk.Tk()
-        AppointmentGUI(main_root, scheduler)
+        AppointmentGUI(main_root, scheduler, user)
         main_root.mainloop()
 
     LoginGUI(root, auth, start_app)
     root.mainloop()
-
     db.close()
 
 if __name__ == "__main__":
